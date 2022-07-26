3f55a3c647e4992c97bb5abc27a859586e6a9c74
yaswanth-vakkala <yaswanthv87@gmail.com>
2022-07-26 09:33:39 +0530

    complete flexbox layout for the books

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 let myLibrary = [
   { title: "jj", author: "lfsd", npages: 123, isRead: "yes" },
   { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
+  { title: "tk", author: "lfsd", npages: 123, isRead: "yes" },
 ];
 
 function Book(title, author, npages, isRead) {
@@ -29,7 +56,12 @@ function displayBooks() {
   myLibrary.forEach(function (curr) {
     // const element = new Book(curr.title, curr.author, curr.npages, curr.isRead);
     const child = document.createElement("div");
-    child.innerHTML = curr.title;
+    child.innerHTML = `
+      <div class="card" >
+        <div><h3>${curr.title}</h3></div>
+        <div><p>${curr.author}</p></div>
+      </div>
+    `;
     area.appendChild(child);
   });
 }
